9f2b45a974a47d59a6fed2d80cd8b64cbe30f888
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-07-29 23:03:04 +0400

    Remove type from env

--- src/Helpers/Env.php
+++ src/Helpers/Env.php
@@ -3,7 +3,7 @@ namespace Meezaan\Microservice\Helpers;
 
 class Env
 {
-    public static function get(string $name, string $default): ?string
+    public static function get(string $name, string $default)
     {
         if (getenv($name)) {
             return getenv($name);
