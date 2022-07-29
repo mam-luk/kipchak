18915ffe7b08ae99f15d02f1a3515ddf1d8fa84f
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-07-30 02:21:18 +0400

    Add exception handler

---
+++ src/Components/Exceptions/Handlers/DefaultHandler.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Meezaan\Microservice\Components\Exceptions\Handlers;
+
+use Psr\Http\Message\ResponseInterface;
+use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpException;
+use Slim\Exception\HttpForbiddenException;
+use Slim\Exception\HttpMethodNotAllowedException;
+use Slim\Exception\HttpNotFoundException;
+use Slim\Exception\HttpNotImplementedException;
+use Slim\Exception\HttpUnauthorizedException;
+use Slim\Handlers\ErrorHandler;
+
+class DefaultHandler extends ErrorHandler
+{
+    public const BAD_REQUEST = 'BAD_REQUEST';
+    public const INSUFFICIENT_PRIVILEGES = 'INSUFFICIENT_PRIVILEGES';
+    public const NOT_ALLOWED = 'NOT_ALLOWED';
+    public const NOT_IMPLEMENTED = 'NOT_IMPLEMENTED';
+    public const RESOURCE_NOT_FOUND = 'RESOURCE_NOT_FOUND';
+    public const SERVER_ERROR = 'SERVER_ERROR';
+    public const UNAUTHENTICATED = 'UNAUTHENTICATED';
+
+    protected function respond(): ResponseInterface
+    {
+        $exception = $this->exception;
+        $statusCode = 500;
+        $type = self::SERVER_ERROR;
+
+        if ($exception instanceof HttpException) {
+            if ($statusCode >= 400) {
+                $statusCode = $exception->getCode();
+            }
+
+
+            if ($exception instanceof HttpNotFoundException) {
+                $type = self::RESOURCE_NOT_FOUND;
+            } elseif ($exception instanceof HttpMethodNotAllowedException) {
+                $type = self::NOT_ALLOWED;
+            } elseif ($exception instanceof HttpUnauthorizedException) {
+                $type = self::UNAUTHENTICATED;
+            } elseif ($exception instanceof HttpForbiddenException) {
+                $type = self::UNAUTHENTICATED;
+            } elseif ($exception instanceof HttpBadRequestException) {
+                $type = self::BAD_REQUEST;
+            } elseif ($exception instanceof HttpNotImplementedException) {
+                $type = self::NOT_IMPLEMENTED;
+            }
+        }
+
+        $description = $exception->getMessage();
+
+        $errorDisplay = [
+            'code' => $statusCode,
+            'status' => $type,
+            'message' => $description,
+        ];
+
+        $payloadDisplay = json_encode($errorDisplay, JSON_PRETTY_PRINT);
+        $response = $this->responseFactory->createResponse($statusCode);
+
+        $response->getBody()->write($payloadDisplay);
+
+        return $response->withHeader('Content-Type', 'application/json');
+
+    }
+}
