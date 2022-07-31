6d02ddefaaa9f5cdec726a07bd374b258c1f0eed
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-07-31 08:08:13 +0400

    Add logger to all controllers

--- src/Components/Controllers/Slim.php
+++ src/Components/Controllers/Slim.php
@@ -2,14 +2,24 @@
 
 namespace Meezaan\Microservice\Components\Controllers;
 
+use Monolog\Logger;
 use Psr\Container\ContainerInterface;
 
 class Slim
 {
+    /**
+     * @var ContainerInterface 
+     */
     protected $container;
+
+    /**
+     * @var Logger
+     */
+    protected $logger;
 
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
+        $this->logger = $this->container->get('logger');
     }
 }
