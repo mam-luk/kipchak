7b246b95a67d6dbd5f73d6338bb88042489231a2
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-07-31 08:15:13 +0400

    Add request helper

--- src/Components/Controllers/Slim.php
+++ src/Components/Controllers/Slim.php
@@ -8,7 +8,7 @@ use Psr\Container\ContainerInterface;
 class Slim
 {
     /**
-     * @var ContainerInterface 
+     * @var ContainerInterface
      */
     protected $container;
 
---
+++ src/Components/Http/Request.php
@@ -0,0 +1,18 @@
+<?php
+namespace Meezaan\Microservice\Components\Http;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+class Request
+{
+    public static function getQueryParam(ServerRequestInterface $request, string $name): mixed
+    {
+        $params = $request->getQueryParams();
+        if (isset($params[$name])) {
+            return $params[$name];
+        }
+
+        return null;
+
+    }
+}
