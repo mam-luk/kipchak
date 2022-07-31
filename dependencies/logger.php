a64edce18dd85a20787a41ac35bd24da04941253
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-07-31 08:02:55 +0400

    Update logger to allow overwriting

--- dependencies/logger.php
+++ dependencies/logger.php
@@ -7,8 +7,25 @@ use Psr\Container\ContainerInterface;
 
 $container->set('logger', function(ContainerInterface $c) {
 $config = $c->get('config');
-$debug = $config->get('api')['debug'];
-$logLevel = $debug === true ? Level::Debug : Level::Info;
+$apiConfig = $config->get('api');
+if (
+    isset($apiConfig['loglevel']) &&
+    in_array(
+        $apiConfig['loglevel'],
+        [
+            Level::Info,
+            Level::Debug,
+            Level::Warning,
+            Level::Alert,
+            Level::Critical,
+            Level::Emergency,
+            Level::Error
+        ])) {
+    $logLevel = $apiConfig['loglevel'];
+} else {
+    $logLevel = $apiConfig['debug'] === true ? Level::Debug : Level::Info;
+}
+
 $log = new Logger('Api');
 $log->pushHandler( new StreamHandler('php://stdout', $logLevel));
 
