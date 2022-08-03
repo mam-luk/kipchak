1e8116b4ba16c1bc1f492224cc97f43106cf9ec7
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-08-03 09:38:18 +0400

    Move files and add caching

--- src/Components/Http/Response.php
+++ src/Components/Http/Response.php
@@ -15,11 +15,18 @@ class Response
             ];
     }
 
-    public static function json(ResponseInterface $response, mixed $data, int $code): ResponseInterface
+    public static function json(ResponseInterface $response, mixed $data, int $code, bool $cache = false, int $cacheTTL = 3600): ResponseInterface
     {
         $response->getBody()->write(
                 json_encode(self::build($data, $code))
         );
+
+        if ($cache) {
+            return $response
+                ->withAddedHeader('Content-Type', 'application/json')
+                ->withAddedHeader('Cache-Control', 'public, must-revalidate, max-age=' . $cacheTTL)
+                ->withStatus($code);
+        }
 
         return $response->withHeader('Content-Type', 'application/json')
             ->withStatus($code);
