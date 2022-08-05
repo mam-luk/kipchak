18809daf326942bf46130416300af008b469204b
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-08-05 10:43:53 +0400

    Add memcached

--- dependencies/cache.php
+++ dependencies/cache.php
@@ -2,10 +2,29 @@
 
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\Adapter\MemcachedAdapter;
+use Memcached;
 
 $container->set('cache_file', function(ContainerInterface $c) {
     $config = $c->get('config')['api'];
     $namespace = $config['name'] ?? 'apiCache';
 
     return new FilesystemAdapter($namespace);
+});
+
+$container->set('cache_memcached', function(ContainerInterface $c) {
+    $configApi = $c->get('config')['api'];
+    $namespace = $configApi['name'] ?? 'apiCache';
+
+    $servers = $c->get('config')['memcached'];
+
+    $memcached = new Memcached($namespace);
+    $memcached->setOption(Memcached::OPT_CONNECT_TIMEOUT, 10);
+    $memcached->setOption(Memcached::OPT_DISTRIBUTION, Memcached::DISTRIBUTION_CONSISTENT);
+    $memcached->setOption(Memcached::OPT_SERVER_FAILURE_LIMIT, 2);
+    $memcached->setOption(Memcached::OPT_REMOVE_FAILED_SERVERS, true);
+    $memcached->setOption(Memcached::OPT_RETRY_TIMEOUT, 1);
+    $memcached->addServers($servers);
+
+    return $memcached;
 });
