1c05689d18f8f2616b64c1759b6bd1dd6c95a280
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-08-05 10:10:52 +0400

    Add global auth middleware

--- middlewares/auth.php
+++ middlewares/auth.php
@@ -1,5 +1,8 @@
 <?php
 
+use Mamluk\Kipchak\Components\Middlewares\AuthJwks;
+use Mamluk\Kipchak\Components\Middlewares\AuthKey;
+
 $container = $app->getContainer();
 $api = $container->get('config')['api'];
 if ($api['auth']['jwks']['enabled'] && isset($request->getHeader('Authorization')[0])) {
