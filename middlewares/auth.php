4ac9cc96d931a12341b1ef173136833ca8a68ede
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-08-08 11:50:25 +0400

    Fix issue

--- middlewares/auth.php
+++ middlewares/auth.php
@@ -4,14 +4,16 @@ use Mamluk\Kipchak\Components\Middlewares\AuthJwks;
 use Mamluk\Kipchak\Components\Middlewares\AuthKey;
 
 $container = $app->getContainer();
-$auth = $container->get('config')['kipchak_auth'];
-if ($auth['jwks']['enabled'] && isset($request->getHeader('Authorization')[0])) {
-    // Enable JWKS globally
-    $app->add(new AuthJwks($container));
-}
+if (isset($container->get('config')['kipchak_auth'])) {
+    $auth = $container->get('config')['kipchak_auth'];
+    if ($auth['jwks']['enabled']) {
+        // Enable JWKS globally
+        $app->add(new AuthJwks($container));
+    }
 
-if ($auth['key']['enabled']) {
-    // Enable Key based auth globally
-    $app->add(new AuthKey($container));
+    if ($auth['key']['enabled']) {
+        // Enable Key based auth globally
+        $app->add(new AuthKey($container));
 
+    }
 }
