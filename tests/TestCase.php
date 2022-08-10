3318a9a485394a9b4b900bfaf82f7f3f3c146507
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-08-10 11:00:46 +0400

    Add base Test case for functional testing of APIs

---
+++ tests/TestCase.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mamluk\Kipchak\Tests;
+
+use PHPUnit\Framework\TestCase as PHPUnit_TestCase;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Psr7\Factory\StreamFactory;
+use Slim\Psr7\Headers;
+use Slim\Psr7\Request as SlimRequest;
+use Slim\Psr7\Uri;
+use Mamluk\Kipchak\Api;
+
+class TestCase extends PHPUnit_TestCase
+{
+
+    /**
+     * @return App
+     */
+    protected function getAppInstance(): App
+    {
+        // Instantiate PHP-DI ContainerBuilder
+        return Api::bootCli();
+    }
+
+    /**
+     * @param string $method
+     * @param string $path
+     * @param array  $headers
+     * @param array  $cookies
+     * @param array  $serverParams
+     * @return Request
+     */
+    protected function createRequest(
+        string $method,
+        string $path,
+        array $headers = ['HTTP_ACCEPT' => 'application/json'],
+        array $cookies = [],
+        array $serverParams = []
+    ): Request {
+        $uri = new Uri('', '', 80, $path);
+        $handle = fopen('php://temp', 'w+');
+        $stream = (new StreamFactory())->createStreamFromResource($handle);
+
+        $h = new Headers();
+        foreach ($headers as $name => $value) {
+            $h->addHeader($name, $value);
+        }
+
+        return new SlimRequest($method, $uri, $h, $cookies, $serverParams, $stream);
+    }
+}
--- tests/Unit/CodesTest.php
+++ tests/Unit/CodesTest.php
@@ -1,8 +1,11 @@
 <?php
-use Mamluk\Kipchak\Tests\Unit;
-use Mamluk\Kipchak\Components\Http\Codes;
 
-class CodesTest extends \PHPUnit\Framework\TestCase
+namespace Mamluk\Kipchak\Tests\Unit;
+
+use Mamluk\Kipchak\Components\Http\Codes;
+use Mamluk\Kipchak\Tests\TestCase;
+
+class CodesTest extends TestCase
 {
     public function testSomething()
     {
