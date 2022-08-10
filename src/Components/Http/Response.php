93046ca28e298ca68246e15a15ef4a1d2ee8a4b0
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-08-10 09:31:13 +0400

    Add slim cache v1

--- src/Components/Http/Response.php
+++ src/Components/Http/Response.php
@@ -18,13 +18,12 @@ class Response
 
     public static function json(ResponseInterface $response, mixed $data, int $code, bool $cache = false, int $cacheTTL = 3600, CacheProvider $provider = new CacheProvider()): ResponseInterface
     {
-        $response->getBody()->write(
-                json_encode(self::build($data, $code))
-        );
+        $json = json_encode(self::build($data, $code));
+        $response->getBody()->write($json);
 
         if ($cache) {
             $response = $provider->withExpires($response, $cacheTTL);
-            $response = $provider->withEtag($response, md5($data));
+            $response = $provider->withEtag($response, $json);
         }
 
         return $response->withHeader('Content-Type', 'application/json')
