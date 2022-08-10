404f28f023a7c3b53d81d9612b8cfaa6de9b83cd
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2022-08-10 09:51:08 +0400

    fix http caching

--- src/Components/Http/Response.php
+++ src/Components/Http/Response.php
@@ -18,15 +18,18 @@ class Response
 
     public static function json(ResponseInterface $response, mixed $data, int $code, bool $cache = false, int $cacheTTL = 3600, CacheProvider $provider = new CacheProvider()): ResponseInterface
     {
-        $json = json_encode(self::build($data, $code));
+        $json = json_encode(self::build($data, $code);
         $response->getBody()->write($json);
 
         if ($cache) {
             $response = $provider->withExpires($response, time() + $cacheTTL);
             $response = $provider->withEtag($response, md5($json));
+            return $response->withHeader('Content-Type', 'application/json')
+                ->withAddedHeader('Cache-Control', 'public, must-revalidate, max-age=' . $cacheTTL)
+                ->withStatus($code);
         }
 
         return $response->withHeader('Content-Type', 'application/json')
             ->withStatus($code);
-    }
+        }
 }
